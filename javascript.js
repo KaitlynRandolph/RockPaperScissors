2b2ab38efdf8c6a6b9304e79603f6dc3eb2ba4c8
KaitlynRandolph <knr0022@auburn.edu>
2022-03-31 17:27:39 -0500

    Wrote function to pick computer choice

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function computerPlay() {
+	let options = ["rock", "paper", "scissors"];
+	let choice = options[Math.floor(Math.random() * 3)];
+	console.log(choice);
+	return choice;
+}
+
+function playRound(playerSelection, computerSelection) {}
+
